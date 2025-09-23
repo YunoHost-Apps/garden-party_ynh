291107ae0d9d1022c69c970405a4b8efa30126ea
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-09-23 12:31:55 +0200

    finish package

--- scripts/_common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#=================================================
-# COMMON VARIABLES AND CUSTOM HELPERS
-#=================================================
-
-_exec_as_app_with_ruby_node() {
-    ynh_exec_as_app env PATH="$path_with_nodejs:$path_with_ruby:$PATH" "$@"
-}
